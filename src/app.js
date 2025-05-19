46227be84527f7ae1d256aa07973ce14933b5069
Romina Verónica Torres <110548507+RomiTorres@users.noreply.github.com>
2025-05-19 17:36:24 +0000

    Creador de dominios con hover

--- src/app.js
+++ src/app.js
@@ -7,5 +7,47 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let pronoun = ["the", "our", "Mrs"];
+  let adj = ["great", "big", "fun"];
+  let noun = ["jogger", "racoon", "dog"];
+  let domin = [".com", ".net", ".ar", ".es"];
+
+
+  function generateDomain(pronoun, adj, noun, domin) {
+    let dominios = "<table class='table table-striped'>";
+
+    for (const i in pronoun) {
+      for (const j in adj) {
+        for (const k in noun) {
+          for (const l in domin) {
+            const pronombres = pronoun[i];
+            const adjetivo = adj[j];
+            const element = noun[k];
+            const dominio = domin[l];
+
+            dominios +=
+              "<tr><td >" +
+              pronombres +
+              adjetivo +
+              element +
+              dominio +
+              " " +
+              "</td></tr>";
+            console.log(dominios);
+          }
+        }
+      }
+    }
+    dominios += "</table>";
+    return dominios;
+  }
+
+
+  const misDominios = document.querySelector("#domain");
+  const dominios = generateDomain(pronoun, adj, noun, domin);
+  misDominios.innerHTML = dominios;
+
   console.log("Hello Rigo from the console!");
 };
+
